780e35618a61d9c104b4ec384515ba910103dc5a
invcble <imonbera13@gmail.com>
2024-05-04 15:37:12 -0400

    Update

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Hello World")
+import whisper
+import os
+
+model = whisper.load_model("whisper_models\\small.en.pt")
+result = model.transcribe("audio.mp3")
+print(result["text"])
+
+os.remove("audio.srt")
+os.remove("audio.txt")
+os.remove("audio.tsv")
+os.remove("audio.vtt")
