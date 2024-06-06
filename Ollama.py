4ffc86b12589ef5aa0daf9b34c4e28d78d61c720
invcble <imonbera13@gmail.com>
2024-06-05 20:30:31 -0400

    update

--- Ollama.py
+++ Ollama.py
@@ -3,9 +3,9 @@ import ollama
 with open("Lecture_transcript.txt", 'rb') as f:
     text = f.read()
 
-instruction = 'Fully parse this transcript of a class lecture of Software Architecture and write a 1000 words note for it as if you were in the class as a student, include details from the whole lecture, DO NOT SKIP ANYTHING ON THE TRANSCRIPT, omit student conversations \n'
+instruction = 'You are a diligent student taking study notes during a class lecture. Your task is to fully parse this transcript of the class lecture and generate a comprehensive and detailed study note. Your notes should be structured, information-rich, and focused on the concepts and key points discussed in the lecture. Do not include any mentions of individuals, their questions, interactions, or any administrative details. Aim for around 1000 words of content that captures the essence of the lectures subject matter.\n'
 
 content = ollama.generate(model='llama3', prompt=instruction + str(text))['response']
 
-with open("Generated_Class_Note.md", 'w') as f:
+with open("MOD_Generated_Class_Note.md", 'w') as f:
     f.write(content)
---
+++ new.py
@@ -0,0 +1,38 @@
+from groq import Groq
+
+with open('API-key-grok.json', 'r') as r:
+    key = r.read()
+
+with open("Lecture_transcript.txt", 'rb') as f:
+    text = f.read()
+    
+Client = Groq(api_key= key)
+
+complete_chat = Client.chat.completions.create(
+    messages=[
+        {
+            "role": "system",
+            "content": "You are a diligent student taking study notes during a class lecture. Your task is to fully parse this transcript of the class lecture and generate a comprehensive and detailed study note. Your notes should be structured, information-rich, and focused on the concepts and key points discussed in the lecture. Do not include any mentions of individuals, their questions, interactions, or any administrative details. Aim for around 1000 words of content that captures the essence of the lectures subject matter."
+
+        },
+        {
+            "role": "user",
+            "content": str(text)[:5000]
+
+        },
+        {
+            "role": "user",
+            "content": str(text)[5001:10000]
+
+        },
+        {
+            "role": "user",
+            "content": str(text)[10001:14000]
+
+        }
+    ],
+    model="mixtral-8x7b-32768",
+    temperature=0
+)
+
+print(complete_chat.choices[0].message.content)
